4c7efd0c1480cb7c308ca9119e510a2be94f9fcd
Erik Morelli <erikm.dev@gmail.com>
2023-08-31 13:05:15 -0300

    Update

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// $(function() { 
+//     let topHeight = -46
+    
+//     let topAnim = setInterval(()=>{
+//         $('#topContainer').css('top', topHeight++)
+//         if(topHeight == 0) clearInterval(topAnim)
+//     },8)
+// });
